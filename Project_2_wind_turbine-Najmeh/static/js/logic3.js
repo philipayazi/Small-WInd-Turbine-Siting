c19c8a380cae272e0a5439d86da3ff103f643f9a
Najmeh dehghani <55862735+nmansoorabadi@users.noreply.github.com>
2020-02-14 03:34:10 -0600

    Add files via upload

--- Project_2_wind_turbine-Najmeh/static/js/logic3.js
+++ Project_2_wind_turbine-Najmeh/static/js/logic3.js
@@ -7,7 +7,13 @@ function createMap(speedMap) {
   id: "mapbox.light",
   accessToken: API_KEY
 });
-const darkmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+const streetMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets",
+  accessToken: API_KEY
+});
+const darkMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
   maxZoom: 18,
   id: "mapbox.dark",
@@ -26,9 +32,10 @@ var myMap= L.map("map",{
 
 const baseMaps = {
   // 'Satellite':satelliteMap,
-  'Average of Wind direction': lightMap,
+  'Grey Map': lightMap,
   // 'Outdoors': outdoorMap,
-  'Average of Wind Speed': darkmap
+  'Street Map': streetMap,
+  'Dark Map' : darkMap
 },
 overlayLayer = {
   'Wind Speed': speedMap,
@@ -62,35 +69,6 @@ legend.onAdd = function (myMap) {
   };
   legend.addTo(myMap); 
 
-  var CanvasLayer = L.GridLayer.extend({
-    createTile: function(coords){
-        // create a <canvas> element for drawing
-        var tile = L.DomUtil.create('canvas', 'leaflet-tile');
-        // setup tile width and height according to the options
-        var size = this.getTileSize();
-        tile.width = size.x;
-        tile.height = size.y;
-        // get a canvas context and draw something on it using coords.x, coords.y and coords.z
-        var ctx = tile.getContext('2d');
-        // return the tile so it can be rendered on screen
-        return tile;
-    }
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 }
 function createMarkers(data) {
@@ -103,10 +81,13 @@ function createMarkers(data) {
         // weight: 1,
         opacity: 0.5,
         fillOpacity:1
-      })
-      // .setRadius(data.mph_avg)
-      .bindPopup(`<html><strong>${item.city}
+      }).bindPopup(`<html><strong>${item.city}
       <br>Average Speed:${item.mph_avg}<br> Average wind direction:${item.deg_avg}</strong></html>`)
+      .on('mouseover',function(ev) {
+        ev.target.openPopup();
+      });
+       
+     
       })
      
       createMap(L.layerGroup(speedMap));
@@ -137,57 +118,7 @@ d3.json(queryurl, function(data) {
 
 
 
+  ['#ffffcc','#ffeda0','#fed976','#feb24c','#fd8d3c','#fc4e2a','#e31a1c','#bd0026','#800026']
 
 
-
-
-
-
-
-
-
-
-  
-  
-// // Create Lagend
-// // var legend = L.control({position: 'bottomright'});
-
-// // legend.onAdd = function (myMap) {
-
-// //     var div = L.DomUtil.create('div', 'info legend'),
-// //     grades = [0, 1,2,3,4,5],
-// //     labels = [];
-// //     for (var i = 0; i < grades.length; i++) {
-      
-// //         div.innerHTML +=
-// //             '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
-// //             grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
-
-// //       }
-
-// //     return div;      
-// //   };
-// //   legend.addTo(myMap); 
-
-
-   
-      
-    
-// // click event to go to the url
-
-//   //   for (var i = 0; i < markers.length; i++) {
-//   //     var a = markers[i];
-//   //     var title = a[2];
-//   //     var link = a[3];
-//   //     var image = a[4];
-//   //  var list = 
-//   //     "<dl>"
-//   //         + "<dt>" + title + "</dt>"
-//   //         + image
-//   //         +
-//   //    "</dl>";
-//   //     var marker = new L.Marker(new L.LatLng(a[0], a[1]), { riseOnHover: true }).bindLabel(list).addTo(map);
-//   //     marker.on("click", function() {
-//   //         window.open(link, "_blank");
-//   //         }
-//   //     );  
+ 
