1d6ae6acbf0a18ab62c47d5fdec9d491eb132a28
Najmeh dehghani <55862735+nmansoorabadi@users.noreply.github.com>
2020-02-14 20:06:29 -0600

    Add files via upload

--- Project_2_wind_turbine-Najmeh/static/js/logic3.js
+++ Project_2_wind_turbine-Najmeh/static/js/logic3.js
@@ -1,48 +1,53 @@
+// Create map object
+    function createMap(speedMap) {
 
-function createMap(speedMap) {
-  // Define maps layers
-  const lightMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  maxZoom: 18,
-  id: "mapbox.light",
-  accessToken: API_KEY
-});
-const streetMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  maxZoom: 18,
-  id: "mapbox.streets",
-  accessToken: API_KEY
-});
-const darkMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  maxZoom: 18,
-  id: "mapbox.dark",
-  accessToken: API_KEY
-});
+      const lightMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+      maxZoom: 18,
+      id: "mapbox.light",
+      accessToken: API_KEY
+    });
+    const streetMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+      maxZoom: 18,
+      id: "mapbox.streets",
+      accessToken: API_KEY
+    });
+    const darkMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+      maxZoom: 18,
+      id: "mapbox.dark",
+      accessToken: API_KEY
+    });
+    const satelliteMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+      maxZoom: 18,
+      id: "mapbox.satellite-v9",
+      accessToken: API_KEY
+    });
 
-var myMap= L.map("map",{
-  center: [37.0902,-95.7129],
+    //Adding tile layer
 
-  
-  zoom: 5,
-  layer : [lightMap,speedMap]
+    var myMap= L.map("map",{
+      center: [37.0902,-95.7129],    
+      zoom: 5,
+      layer : [lightMap,speedMap]
+    });
 
-});
-// var cities = L.layerGroup(cityMarkers);
 
-const baseMaps = {
-  // 'Satellite':satelliteMap,
-  'Grey Map': lightMap,
-  // 'Outdoors': outdoorMap,
-  'Street Map': streetMap,
-  'Dark Map' : darkMap
-},
-overlayLayer = {
-  'Wind Speed': speedMap,
-};
+  const baseMaps = {
+    // 'Satellite':satelliteMap,
+    'Grey Map': lightMap,
+    // 'Outdoors': outdoorMap,
+    'Street Map': streetMap,
+    'Dark Map' : darkMap,
+    'satellite-v9':satelliteMap
+  },
+  overlayLayer = {
+    'Wind Speed': speedMap,
+  };
 
-//   Create a layer control containing our baseMaps and overlay layer of the earthquake geoJson
-
+//   Create a layer control containing our baseMaps and overlay layer 
   L.control.layers(baseMaps, overlayLayer, {
     collapsed: true
   }).addTo(myMap);
@@ -50,57 +55,59 @@ overlayLayer = {
   myMap.addLayer(speedMap);
 
 // Create Lagend
-var legend = L.control({position: 'bottomright'});
+    var legend = L.control({position: 'bottomright'});
+    legend.onAdd = function (myMap) {
+        var div = L.DomUtil.create('div', 'info legend'),
+        grades = [0, 1,2,3,4,5],
+        labels = [];
+        for (var i = 0; i < grades.length; i++) {
+            div.innerHTML +=
+                '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+                grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+          }
+        return div;      
+      };
+      legend.addTo(myMap); 
+    }
 
-legend.onAdd = function (myMap) {
-
-    var div = L.DomUtil.create('div', 'info legend'),
-    grades = [0, 1,2,3,4,5],
-    labels = [];
-    for (var i = 0; i < grades.length; i++) {
-      
-        div.innerHTML +=
-            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
-            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
-
-      }
-
-    return div;      
-  };
-  legend.addTo(myMap); 
-
-
-}
+//Create Markers
 function createMarkers(data) {
+  
+      // L.CircleMarker = L.CircleMarker.extend({
+      //   isSemicircle: function () { return false; }
+      // });
 
       const speedMap=data.map(item =>{
         return L.circleMarker(item.loc,
         {radius: item.mph_avg*8,
         fillColor:getColor(item.mph_avg),
         color: "#000",
-        // weight: 1,
+        weight: 1,
         opacity: 0.5,
-        fillOpacity:1
-      }).bindPopup(`<html><strong>${item.city}
-      <br>Average Speed:${item.mph_avg}<br> Average wind direction:${item.deg_avg}</strong></html>`)
-      .on('mouseover',function(ev) {
-        ev.target.openPopup();
-      });
-       
-     
+        fillOpacity:1,
+        startAngle:0,
+        stopAngle:30
       })
-     
+      .bindPopup(`<html><strong>${item.city}
+      <br>Average Speed:${item.mph_avg}<br> Average wind direction:${item.deg_avg}</strong></html>`)
+      .on('mouseover', function (e) {
+            this.openPopup();
+      })
+      .on('mouseout', function (e) {
+          this.closePopup();
+      });    
+      }) 
       createMap(L.layerGroup(speedMap));
-  // });
-
 }
 
-var queryurl="http://127.0.0.1:5000/";
-d3.json(queryurl, function(data) {
-    console.log(data);
-    createMarkers(data);
-                 
-});
+// Calling json data fron Flask API
+
+  var queryurl="http://127.0.0.1:5000/";
+  d3.json(queryurl, function(data) {
+      console.log(data);
+      createMarkers(data);
+                  
+  });
 
   function getColor(d) {
       return d > 5 ? '#67000d' :
@@ -115,10 +122,8 @@ d3.json(queryurl, function(data) {
             d > 0.5  ? '#ffffcc' :
                       '#FFEDA0';
   }
-
-
-
-  ['#ffffcc','#ffeda0','#fed976','#feb24c','#fd8d3c','#fc4e2a','#e31a1c','#bd0026','#800026']
+//Color code
+  // ['#ffffcc','#ffeda0','#fed976','#feb24c','#fd8d3c','#fc4e2a','#e31a1c','#bd0026','#800026']
 
 
  
